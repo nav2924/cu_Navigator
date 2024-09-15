1796fb1a6356d6f31a11fea3b4d8a06e54e52f9e
nav2924 <navi29012004@gmail.com>
2024-09-15 21:33:54 +0530

    Marker

--- client/src/components/MapView.jsx
+++ client/src/components/MapView.jsx
@@ -9,14 +9,10 @@ import useGeoLocation from "../hooks/useGeoLocation";
 import { useLocation } from "react-router-dom";
 
 const userLocationIcon = new L.Icon({
-  iconUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-red.png", // Absolute URL
+  iconUrl: "/marker.jpeg", // Local URL
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png", // Add shadow
-  shadowSize: [41, 41],
 });
 
 const RoutingMachine = ({ start, end }) => {
