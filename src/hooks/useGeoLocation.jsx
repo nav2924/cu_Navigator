e7f9dfb7417336baa5f57c818fd61d4de2e1e0f2
nav2924 <navi29012004@gmail.com>
2024-09-17 21:43:58 +0530

    Block 6 added

--- src/hooks/useGeoLocation.jsx
+++ src/hooks/useGeoLocation.jsx
@@ -1,44 +1,46 @@
-import React, { useEffect, useState } from "react";
+// Sample useGeoLocation Hook
+import { useState, useEffect } from "react";
 
 const useGeoLocation = () => {
   const [location, setLocation] = useState({
-    loading: true,  
-    coordinates: { lat: "", lng: "" },
+    loading: true,
+    coordinates: { lat: null, lng: null },
     error: null,
   });
 
-  const onSuccess = (position) => {
-    setLocation({
-      loading: false,
-      coordinates: {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      },
-      error: null,
-    });
-  };
-
-  const onError = (error) => {
-    setLocation({
-      loading: false,
-      coordinates: { lat: "", lng: "" },
-      error: error.message, 
-    });
-  };
-
   const getLocation = () => {
-    if ("geolocation" in navigator) {
-      setLocation((prevState) => ({ ...prevState, loading: true })); // Set loading to true before fetching location
-      navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    } else {
-      onError({
-        message: "Geolocation not supported by your browser",
+    if (!navigator.geolocation) {
+      setLocation({
+        loading: false,
+        coordinates: { lat: null, lng: null },
+        error: "Geolocation not supported",
       });
+      return;
     }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLocation({
+          loading: false,
+          coordinates: {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          },
+          error: null,
+        });
+      },
+      (error) => {
+        setLocation({
+          loading: false,
+          coordinates: { lat: null, lng: null },
+          error: error.message,
+        });
+      }
+    );
   };
 
   useEffect(() => {
-    getLocation(); // Optionally get location on component mount
+    getLocation();
   }, []);
 
   return { location, getLocation };
